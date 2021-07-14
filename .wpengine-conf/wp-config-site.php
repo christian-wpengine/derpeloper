679c03028c7e308b293eab7a66d02da3109df8fe
Christian Romero <christian.romero@wpengine.com>
2021-07-14 15:49:05 -0500

    add gitignore and all default files

---
+++ .wpengine-conf/wp-config-site.php
@@ -0,0 +1,5 @@
+<?php
+# GENERATED AUTOMATICALLY ON 2021-07-14 14:53:29.597443 -0500 CDT m=+45.460176486
+$table_prefix = "wp_";
+$wpengine_primary_original_domain = "derpeloper.wpengine.com";
+$wpengine_primary_original_ip_address = "35.231.223.215";
